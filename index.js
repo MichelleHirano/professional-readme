1b1c6af847ab2f23b63ba35512fca107fda5ea0f
MHirano <michelle@hiranohomes.com>
2021-04-10 21:11:03 -0700

    console log check

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-// TODO: Include packages needed for this application
+ //TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require("inquirer");
+const util = require('util');
 
-// const inquirer = require("inquirer");
-
-// // TODO: Create an array of questions for user input
-// const questions = [];
+// TODO: Create an array of questions for user input
+ const questions = [];
 
 // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-// function init() {}
+function init() {}
 
 // Function call to initialize app
-// init();
-
-const inquirer = require('inquirer');
-console.log(inquirer);
+init();
